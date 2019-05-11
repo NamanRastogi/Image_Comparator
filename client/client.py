8ea9192bd644f05efd80e67b5eb7ec5568374eb6
Naman Rastogi <naman.rastogi.52@gmail.com>
2019-05-12 01:55:15 +0530

    Refactoring

--- client/client.py
+++ client/client.py
@@ -1,39 +1,56 @@
+#!/usr/bin/env python3
+
 import logging
+import os
 import uuid
 
 import grpc
-
 from PIL import Image
+from tkinter import filedialog
 
 from commons import image_similarity_pb2
 from commons import image_similarity_pb2_grpc
 
-
-def input_image(image_path):
-    # logger
-    image_data = Image.open(image_path, 'r').resize((512, 512), Image.BICUBIC)
-    return image_data.tobytes(encoder_name='raw')
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(name)s : %(message)s')
 
 
-def run():
-    logger = logging.getLogger("Client")
-    logger.setLevel(logging.DEBUG)
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = image_similarity_pb2_grpc.ImageSimilarityStub(channel)
-        req_id = str(uuid.uuid4())
-        image_1 = input_image("/home/naman/Documents/ImgComparator/client/im1.jpg")
-        image_2 = input_image("/home/naman/Documents/ImgComparator/client/im2.jpg")
-        logger.info("Requesting image similarity! Request ID: {}".format(req_id))
-        response = stub.get_image_similarity(image_similarity_pb2.Images(
-            request_id=req_id,
-            image_1=image_1,
-            image_2=image_2))
-        resp_id = response.response_id
-        image_similarity_val = response.similarity_val
-        logger.info("Response received! Response ID: {} Similarity: {}".format(resp_id, image_similarity_val))
-        print("Similarity: {}".format(image_similarity_val))
+class Client:
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.DEBUG)
+
+    @staticmethod
+    def select_image(title='Select image'):
+        return filedialog.askopenfilename(
+            initialdir=os.getcwd(),
+            title=title,
+            filetypes=(('JPEG files', '*.jpg'),
+                       ('PNG file', '*.png')))
+
+    @staticmethod
+    def read_image(image_path):
+        image_data = Image.open(image_path, 'r').resize((512, 512), Image.BICUBIC)
+        return image_data.tobytes(encoder_name='raw')
+
+    def query_server(self, ip, port):
+        self.logger.info(f'Connecting {ip}:{port}')
+        with grpc.insecure_channel(f'{ip}:{port}') as channel:
+            stub = image_similarity_pb2_grpc.ImageSimilarityStub(channel)
+            req_id = str(uuid.uuid4())
+            image_1 = self.read_image(self.select_image('Select first image'))
+            image_2 = self.read_image(self.select_image('Select second image'))
+            self.logger.info(f'Requesting image similarity! Request ID: {req_id}')
+            response = stub.get_image_similarity(image_similarity_pb2.Images(
+                request_id=req_id,
+                image_1=image_1,
+                image_2=image_2))
+            resp_id = response.response_id
+            image_similarity_val = response.similarity_val
+            self.logger.info(f'Response received! Response ID: {resp_id} Similarity: {image_similarity_val}')
+            return image_similarity_val
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s")
-    run()
+    client = Client()
+    val = client.query_server('localhost', '50051')
+    print(f'Similarity: {val}')
--- server/server.py
+++ server/server.py
@@ -1,15 +1,12 @@
-"""Server.
-"""
+#!/usr/bin/env python3
 
 import logging
 import time
 import uuid
-
 from concurrent import futures
 
 import grpc
 import numpy as np
-
 from PIL import Image
 
 from img_to_vec import Img2Vec
@@ -18,11 +15,13 @@ from commons import image_similarity_pb2_grpc
 
 _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(name)s : %(message)s')
 
-class ImageSimilarityServicerImpl(image_similarity_pb2_grpc.ImageSimilarityServicer):
+
+class _ImageSimilarityServicerImpl(image_similarity_pb2_grpc.ImageSimilarityServicer):
 
     def __init__(self):
-        self.logger = logging.getLogger("Server")
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.logger.setLevel(logging.DEBUG)
         self.img2vec = Img2Vec()
 
@@ -36,29 +35,36 @@ class ImageSimilarityServicerImpl(image_similarity_pb2_grpc.ImageSimilarityServi
         return self.compute_vec_similarity(vec_1, vec_2)
 
     def get_image_similarity(self, request, context):
-        self.logger.info("Request received! Request ID: {}".format(request.request_id))
+        self.logger.info(f'Request received! Request ID: {request.request_id}')
         resp_id = str(uuid.uuid4())
         image_1 = Image.frombytes(mode='RGB', size=(512, 512), data=request.image_1, decoder_name='raw')
         image_2 = Image.frombytes(mode='RGB', size=(512, 512), data=request.image_2, decoder_name='raw')
         similarity_val = self.compute_image_similarity(image_1, image_2)
-        self.logger.info("Sending response! Response ID: {} Similarity: {}".format(resp_id, similarity_val))
+        self.logger.info(f'Sending response! Response ID: {resp_id} Similarity: {similarity_val}')
         return image_similarity_pb2.Similarity(
             response_id=resp_id,
             similarity_val=similarity_val)
 
 
-def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    image_similarity_pb2_grpc.add_ImageSimilarityServicer_to_server(ImageSimilarityServicerImpl(), server)
-    server.add_insecure_port('[::]:50051')
-    server.start()
-    try:
-        while True:
-            time.sleep(_ONE_DAY_IN_SECONDS)
-    except KeyboardInterrupt:
-        server.stop(0)
+class Server:
+    def __init__(self):
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+        image_similarity_pb2_grpc.add_ImageSimilarityServicer_to_server(_ImageSimilarityServicerImpl(), self.server)
+
+    def start(self, port):
+        self.server.add_insecure_port(f'[::]:{port}')
+        self.server.start()
+        self.logger.info(f'Server started at port {port}')
+        try:
+            while True:
+                time.sleep(_ONE_DAY_IN_SECONDS)
+        except KeyboardInterrupt:
+            self.server.stop(0)
+            self.logger.info('Server stopped!')
 
 
 if __name__ == '__main__':
-    logging.basicConfig(format="%(asctime)s %(levelname)s %(message)s")
-    serve()
+    server = Server()
+    server.start(port='50051')
